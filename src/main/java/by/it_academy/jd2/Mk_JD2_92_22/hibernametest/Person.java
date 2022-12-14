b6f26e202927a04bf65352d21455358a99a816c7
VicRum <vicrum93@gmail.com>
2022-12-14 21:05:10 +0300

    after lesson about hibernate

---
+++ src/main/java/by/it_academy/jd2/Mk_JD2_92_22/hibernametest/Person.java
@@ -0,0 +1,44 @@
+package by.it_academy.jd2.Mk_JD2_92_22.hibernametest;
+
+public class Person {
+
+    private int age;
+
+    private int id;
+
+    private char name;
+
+    private char surname;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public char getName() {
+        return name;
+    }
+
+    public void setName(char name) {
+        this.name = name;
+    }
+
+    public char getSurname() {
+        return surname;
+    }
+
+    public void setSurname(char surname) {
+        this.surname = surname;
+    }
+}
